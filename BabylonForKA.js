b94a4ad62f3d93b1466a52c44eb6262022307197
Dipper98 <134005854+Dip98@users.noreply.github.com>
2025-01-10 14:18:01 -0800

    Create BabylonForKA.js

---
+++ BabylonForKA.js
@@ -0,0 +1,15 @@
+function setupBabylon(){
+        window.addEventListener('DOMContentLoaded', function(){
+                var canvas = document.getElementById('renderCanvas');
+                canvas.getContext = function(type, data) {
+                        data.preserveDrawingBuffer = true;
+                        return HTMLCanvasElement.prototype.getContext.call(canvas, type, data);
+                }
+        });
+        const engine = new BABYLON.Engine(canvas, true);
+}
+const scene = run();
+        
+engine.runRenderLoop(function(){
+        scene.render();
+});
