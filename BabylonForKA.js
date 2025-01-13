11c51a04c45a1606ae4345b1ba1b74181abfabb3
Dipper98 <134005854+Dip98@users.noreply.github.com>
2025-01-13 11:31:31 -0800

    Update BabylonForKA.js

--- BabylonForKA.js
+++ BabylonForKA.js
@@ -1,17 +1,22 @@
+function a(){
 var BabylonForKA = {
         run: function(){}
-};
+    };
 window.addEventListener('DOMContentLoaded', function(){
         var canvas = document.getElementById('renderCanvas');
         canvas.getContext = function(type, data) {
                 data.preserveDrawingBuffer = true;
                 return HTMLCanvasElement.prototype.getContext.call(canvas, type, data);
         }
-});
-function renderBabylon(){
+        BabylonForKA.run = function(){
+            const scene = new BABYLON.Scene(engine);
+            scene.clearColor = new BABYLON.Color3(0, 0, 0)
+            return scene;
+        }
         const engine = new BABYLON.Engine(canvas, true);
         const scene = BabylonForKA.run();
         engine.runRenderLoop(function(){
                 scene.render();
         });
+});
 }
